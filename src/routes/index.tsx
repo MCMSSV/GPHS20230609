9330bf1a6565498bcd4cabf9082b8bdfefcb597b
MS___ <72972052+MCMSSV@users.noreply.github.com>
2023-12-27 22:21:56 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,20 +9,20 @@ export default function Home() {
         {" "}
         <a href="https://www.youtube.com/watch?v=uGf3HZkqMHA" target="_blank">
           10월2주차 우리반 뉴스
-        </a>{" "}            /
+        </a>{" "}            /          
         {" "}
         <a href="https://youtu.be/asRW4bwZnz8" target="_blank">
           11월2주차 우리반 뉴스
-        </a>{" "}            /
+        </a>{" "}            /          
         {" "}
         <a href="https://youtu.be/Ys9d6qjpKSY" target="_blank">
           12월2주차 우리반 뉴스
-        </a>{" "}            /
+        </a>{" "}            /          
         {" "}
         <a href="https://youtu.be/JdkX_hDIhmc" target="_blank">
           졸업영상
-        </a>{" "}            /
-        
+        </a>{" "}            /          
+         
       </p>
     </main>
   );
