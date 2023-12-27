21dc5bee436bb7b525383a77340ea5a723210f2c
MS___ <72972052+MCMSSV@users.noreply.github.com>
2023-12-27 22:15:36 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,13 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>김포호수초 2023학년도 6학년 9반</Title>
+      <h1>영상</h1>
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        <a href="https://www.youtube.com/watch?v=uGf3HZkqMHA" target="_blank">
+          10월2주차 우리반 뉴스
         </a>{" "}
         to learn how to build SolidStart apps.
       </p>
