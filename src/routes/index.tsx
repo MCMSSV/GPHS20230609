b91bc6b002641cd7d89688a0911669968f31b1f3
MS___ <72972052+MCMSSV@users.noreply.github.com>
2023-12-27 22:19:52 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,14 +5,25 @@ export default function Home() {
   return (
     <main>
       <Title>김포호수초 2023학년도 6학년 9반</Title>
-      <h1>영상</h1>
-      <Counter />
+      <h1>김포호수초 2023학년도 6학년 9반 영상</h1>
       <p>
-        Visit{" "}
+        {" "}
         <a href="https://www.youtube.com/watch?v=uGf3HZkqMHA" target="_blank">
           10월2주차 우리반 뉴스
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        </a>{" "}            /
+        {" "}
+        <a href="https://youtu.be/asRW4bwZnz8" target="_blank">
+          11월2주차 우리반 뉴스
+        </a>{" "}            /
+        {" "}
+        <a href="https://youtu.be/Ys9d6qjpKSY" target="_blank">
+          12월2주차 우리반 뉴스
+        </a>{" "}            /
+        {" "}
+        <a href="https://youtu.be/JdkX_hDIhmc" target="_blank">
+          졸업영상
+        </a>{" "}            /
+        
       </p>
     </main>
   );
